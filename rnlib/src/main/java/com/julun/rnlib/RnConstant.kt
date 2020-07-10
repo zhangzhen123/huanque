2f79948c6261005761d99b1c7b5ca8e740342ab0
zhangzhen <z6998796@qq.com>
2020-07-10 19:23:19 +0800

    主页跳转个人主页

--- rnlib/src/main/java/com/julun/rnlib/RnConstant.kt
+++ rnlib/src/main/java/com/julun/rnlib/RnConstant.kt
@@ -4,4 +4,11 @@ object RnConstant {
     const val MODULE_NAME:String="moduleName"
 
     const val INITIAL_PROPERTIES:String="initialProperties"
+
+
+    const val PERSONAL_HOMEPAGE:String="PersonalHomePage"
+
+    const val MINE_HOMEPAGE:String="MineHomePage"
+
+
 }
