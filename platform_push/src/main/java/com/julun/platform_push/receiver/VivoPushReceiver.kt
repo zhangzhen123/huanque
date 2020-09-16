c8ea7ca81791a870315356d4b9c1ecbea5da51e6
zhangzhen <z6998796@qq.com>
2020-09-16 15:29:28 +0800

    Vivo推送适配点击跳转

--- platform_push/src/main/java/com/julun/platform_push/receiver/VivoPushReceiver.kt
+++ platform_push/src/main/java/com/julun/platform_push/receiver/VivoPushReceiver.kt
@@ -1,7 +1,10 @@
 package com.julun.platform_push.receiver
 
 import android.content.Context
+import com.julun.huanque.common.suger.logger
 import com.vivo.push.model.UPSNotificationMessage
+import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
 
 /**
  *@创建者   dong
@@ -11,7 +14,16 @@ import com.vivo.push.model.UPSNotificationMessage
 class VivoPushReceiver : io.rong.push.platform.vivo.VivoPushMessageReceiver() {
 
     override fun onNotificationMessageClicked(context: Context?, message: UPSNotificationMessage?) {
+        logger("onNotificationMessageClicked=${message}")
         super.onNotificationMessageClicked(context, message)
+        val pushData=message?.params?.get("appData")
+        if(pushData!=null&&context!=null){
+            //vivo会默认启动启动页 导致会打开首页  所以这里一定要延迟打开相应页面 不然会被首页覆盖掉
+            Observable.timer(700,TimeUnit.MILLISECONDS).subscribe {
+                RPushUtil.parseJson(pushData,context)
+            }
+
+        }
     }
 
     override fun onReceiveRegId(context: Context?, token: String?) {
