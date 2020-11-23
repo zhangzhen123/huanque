57db00050e87a9e607fe93df5c8f9aea2bb9d027
zhangzhen <z6998796@qq.com>
2020-11-23 19:36:29 +0800

    改进网格内分割的计算方式 修复一些场景分割不均的问题

--- common/src/main/java/com/julun/huanque/common/widgets/recycler/decoration/GridLayoutSpaceItemDecoration2.java
+++ common/src/main/java/com/julun/huanque/common/widgets/recycler/decoration/GridLayoutSpaceItemDecoration2.java
@@ -6,16 +6,22 @@ import android.view.View;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.julun.huanque.common.utils.ULog;
+
+import java.util.logging.Logger;
+
 /**
  * @author zhangzhen
  * @data 2018/12/27
  * <p>
  * GridLayoutManager的万能分隔 边界处不带分割版
+ *
+ * 于2020/11/23 改进算法 修复spanCount>2时分割不均异常的bug
  **/
 
 
 public class GridLayoutSpaceItemDecoration2 extends RecyclerView.ItemDecoration {
-//    Logger logger = ULog.getLogger("GridLayoutSpaceItemDecoration2");
+    Logger logger = ULog.Companion.getLogger("GridLayoutSpaceItemDecoration2");
 
     private int space;
 
@@ -26,11 +32,17 @@ public class GridLayoutSpaceItemDecoration2 extends RecyclerView.ItemDecoration 
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
         GridLayoutManager layoutManager = (GridLayoutManager) parent.getLayoutManager();
-        final GridLayoutManager.LayoutParams lp = (GridLayoutManager.LayoutParams) view.getLayoutParams();
+        if(layoutManager==null){
+            return;
+        }
+//        final GridLayoutManager.LayoutParams lp = (GridLayoutManager.LayoutParams) view.getLayoutParams();
         final int childPosition = parent.getChildAdapterPosition(view);
         final int spanCount = layoutManager.getSpanCount();
         int total = parent.getAdapter().getItemCount();
+        int spanSize = layoutManager.getSpanSizeLookup().getSpanSize(childPosition);
+        int spanIndex=layoutManager.getSpanSizeLookup().getSpanIndex(childPosition,spanCount);
         int spanGroupIndex = layoutManager.getSpanSizeLookup().getSpanGroupIndex(childPosition, spanCount);
+        ; // view 所在的列
         int lastGIndex = layoutManager.getSpanSizeLookup().getSpanGroupIndex(total - 1, spanCount);
         if (layoutManager.getOrientation() == RecyclerView.VERTICAL) {
 //            int spanIndex=layoutManager.getSpanSizeLookup().getSpanIndex(childPosition, spanCount); //同lp.getSpanIndex()
@@ -38,28 +50,32 @@ public class GridLayoutSpaceItemDecoration2 extends RecyclerView.ItemDecoration 
             if (spanGroupIndex == 0) {//第一排
                 outRect.top = 0;
             }
-//            logger.info("ChildCount:"+total+" lp.getSpanIndex()="+lp.getSpanIndex()+"spanIndex:"+spanIndex);
-//            logger.info("spanGroupIndex:"+spanGroupIndex+" lastGIndex:"+lastGIndex+" spanCount="+spanCount+"lp.getSpanSize()="+lp.getSpanSize());
+//            logger.info("childPosition=" + childPosition + " spanGroupIndex:" + spanGroupIndex + " spanCount=" + spanCount +
+//                    " spanSize=" + spanSize + "spanIndex=" + spanIndex);
             if (spanGroupIndex == lastGIndex) {//最后一排
                 outRect.bottom = 0;
             } else {
                 outRect.bottom = space;
             }
             //这里忽略和合并项的问题，只考虑占满和单一的问题
-            if (lp.getSpanSize() == spanCount) {//占满
+            if (spanSize == spanCount) {//占满
                 outRect.left = 0;
                 outRect.right = 0;
             } else {
-                outRect.left = (int) (((float) (spanCount - lp.getSpanIndex())) / spanCount * space);
-                outRect.right = (int) (((float) space * (spanCount + 1) / spanCount) - outRect.left);
-                if (lp.getSpanIndex() == 0) {//第一列
-                    outRect.left = 0;
-                } else if (lp.getSpanIndex() == spanCount - 1) {//最后一列
-                    outRect.right = 0;
-                }
+//                outRect.left = (int) (((float) (spanCount - lp.getSpanIndex())) / spanCount * space);
+//                outRect.right = (int) (((float) space * (spanCount + 1) / spanCount) - outRect.left);
+//                if (lp.getSpanIndex() == 0) {//第一列
+//                    outRect.left = 0;
+//                } else if (lp.getSpanIndex() == spanCount - 1) {//最后一列
+//                    outRect.right = 0;
+//                }
+
+                outRect.left = spanIndex * space / spanCount; // column * (列间距 * (1f / 列数))
+                outRect.right = space - (spanIndex + 1) * space / spanCount; // 列间距 - (column + 1) * (列间距 * (1f /列数))
+                logger.info("childPosition=" + childPosition + " outRect.left" + outRect.left + " outRect.right" + outRect.right);
             }
         } else {
-            if (layoutManager.getSpanSizeLookup().getSpanGroupIndex(childPosition, spanCount) == 0) {//第一排的需要left
+            if (spanGroupIndex == 0) {//第一排的需要left
                 outRect.left = 0;
             }
             if (spanGroupIndex == lastGIndex) {//最后一排的
@@ -68,17 +84,20 @@ public class GridLayoutSpaceItemDecoration2 extends RecyclerView.ItemDecoration 
                 outRect.right = space;
             }
             //这里忽略和合并项的问题，只考虑占满和单一的问题
-            if (lp.getSpanSize() == spanCount) {//占满
+            if (spanSize == spanCount) {//占满
                 outRect.top = 0;
                 outRect.bottom = 0;
             } else {
-                outRect.top = (int) (((float) (spanCount - lp.getSpanIndex())) / spanCount * space);
-                outRect.bottom = (int) (((float) space * (spanCount + 1) / spanCount) - outRect.top);
-                if (lp.getSpanIndex() == 0) {
-                    outRect.top = 0;
-                } else if (lp.getSpanIndex() == spanCount - 1) {
-                    outRect.bottom = 0;
-                }
+//                outRect.top = (int) (((float) (spanCount - lp.getSpanIndex())) / spanCount * space);
+//                outRect.bottom = (int) (((float) space * (spanCount + 1) / spanCount) - outRect.top);
+//                if (lp.getSpanIndex() == 0) {
+//                    outRect.top = 0;
+//                } else if (lp.getSpanIndex() == spanCount - 1) {
+//                    outRect.bottom = 0;
+//                }
+
+                outRect.top = spanIndex * space / spanCount; // column * (列间距 * (1f / 列数))
+                outRect.bottom = space - (spanIndex + 1) * space / spanCount; // 列间距 - (column + 1) * (列间距 * (1f /列数))
             }
         }
 
