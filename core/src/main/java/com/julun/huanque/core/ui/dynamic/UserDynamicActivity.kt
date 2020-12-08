2e0b9382e1db4ecf40336931184ee4b535b9aa0a
zhangzhen <z6998796@qq.com>
2020-12-08 10:25:06 +0800

    个人动态页面发布按钮显隐修复

--- core/src/main/java/com/julun/huanque/core/ui/dynamic/UserDynamicActivity.kt
+++ core/src/main/java/com/julun/huanque/core/ui/dynamic/UserDynamicActivity.kt
@@ -105,9 +105,11 @@ class UserDynamicActivity : BaseVMActivity<UserDynamicViewModel>() {
         if (isMe) {
             headerPageView.initHeaderView(titleTxt = "我的动态")
             dynamicAdapter.showType = DynamicListAdapter.POST_LIST_ME
+            publish_dynamic.show()
         } else {
             headerPageView.initHeaderView(titleTxt = "Ta的动态")
             dynamicAdapter.showType = DynamicListAdapter.POST_LIST_OTHER
+            publish_dynamic.hide()
         }
         headerPageView.imageViewBack.onClickNew {
             finish()
@@ -123,9 +125,15 @@ class UserDynamicActivity : BaseVMActivity<UserDynamicViewModel>() {
         (postList.itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
         dynamicAdapter.headerWithEmptyEnable = true
         postList.adapter = dynamicAdapter
-        dynamicAdapter.setEmptyView(MixedHelper.getEmptyView(this, "暂无动态，快去发一条吧~", btnTex = "去发布", onClick = View.OnClickListener {
-            ARouter.getInstance().build(ARouterConstant.PUBLISH_STATE_ACTIVITY).navigation()
-        }))
+        dynamicAdapter.setEmptyView(
+            MixedHelper.getEmptyView(
+                this,
+                "暂无动态，快去发一条吧~",
+                btnTex = "去发布",
+                onClick = View.OnClickListener {
+                    ARouter.getInstance().build(ARouterConstant.PUBLISH_STATE_ACTIVITY).navigation()
+                })
+        )
 
         postList.isNestedScrollingEnabled = false
 
@@ -319,7 +327,10 @@ class UserDynamicActivity : BaseVMActivity<UserDynamicViewModel>() {
      * @param show  true 显示发布按钮  false 隐藏发布按钮
      */
     private fun showOrHidePublish(show: Boolean) {
-
+        if (!isMe) {
+            publish_dynamic.hide()
+            return
+        }
         if (show) {
             if (mShowPublishAnimation?.isRunning == true) {
                 //动画正在执行，什么都不操作
